f52767652c612f560680f49713b510db254bd26b
BlakeMuchmore01 <125611059+BlakeMuchmore01@users.noreply.github.com>
2023-09-25 11:41:30 +1000

    Adding log functionality and initial starting code

--- LabAssessment2.m
+++ LabAssessment2.m
@@ -1,24 +1,21 @@
 %% Main Function
-% CoBot Blackjack Table
+% CoBot Blackjack Table Simulation
 function LabAssessment2()
     clear; clc; clf; % Clearing workspace, command window, and figures
+    % profile on; % Profiling the code
+
+    % Creating log file and setting command window level
+    L = log4matlab('logFile.log');
+    L.SetCommandWindowLevel(L.DEBUG);
 
     figure 1; % Creating figure to simulate robots
     % Spawning the robots and surrounding environment
-    
+   
 
 
     figure 2; % Creating figure for GUI
     % Creating the app GUI object
 
-
-
-
-
-
-
-
-    
 
 
 end
