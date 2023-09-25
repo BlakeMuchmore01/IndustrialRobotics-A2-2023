8acf95406bc4286613d84a50e17850f5544dfdf4
BlakeMuchmore01 <125611059+BlakeMuchmore01@users.noreply.github.com>
2023-09-25 11:26:27 +1000

    Added task context to readme

--- LabAssessment2.m
+++ LabAssessment2.m
@@ -1,40 +1,15 @@
 %% Main Function
 % CoBot Blackjack Table
+function LabAssessment2()
+    clear; clc; clf; % Clearing workspace, command window, and figures
+
+    figure 1; % Creating figure to simulate robots
+    % Spawning the robots and surrounding environment
+    
 
 
-
-function LabAssessment2
-    % Figure 1 - Robot Environment
-        % Spawning the robots and environment
-        % Active collision avoidance
-        % Reacting to GUI
-        % Reacting to object entering unsafe zone
-        % Simulation of blackjack game
-
-    % Figure 2 - GUI Environment
-        % E-stop button
-        % Continue/Start game button
-        % Teach Functionality
-        % End game button
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    figure 2; % Creating figure for GUI
+    % Creating the app GUI object
 
 
 
