5d5e5f866b708eec7543869a9402a54b9bc78efc
BlakeMuchmore01 <125611059+BlakeMuchmore01@users.noreply.github.com>
2023-10-07 13:55:42 +1100

    creating aubo ply files and testing file for models

--- @AuboI5/AuboI5.m
+++ @AuboI5/AuboI5.m
@@ -41,6 +41,7 @@ classdef AuboI5 < RobotBaseClass
 
             % Plotting the Aubo i5 and associated ply models
             self.model.plot(self.initialJointAngles,'noname','noshadow','nowrist','nobase','notiles');
+            self.PlotAndColourRobot();
 
             % Logging the creation of the Aubo i5
             L.mlog = {L.DEBUG,'AuboI5','Aubo i5 object created within the workspace'};
---
+++ modeltesting.m
@@ -0,0 +1,28 @@
+clf; clear; clc;
+model = 1;
+
+
+if model == 1
+    % Creating log file and setting command window level
+    L = log4matlab('logFile.log');
+    L.SetCommandWindowLevel(L.DEBUG);
+    
+    % Creating 2F-85 gripper and attaching it to the Aubo i5 end-effector
+    twoFingeredGripper = []; % Creating cell structure to store gripper fingers
+    for i = 1:1
+        twoFingeredGripper{i} = TwoFingeredGripper(eye(4),i,L);
+    end
+    
+    twoFingeredGripper{1}.model.teach(zeros(1,5));
+end
+
+if model == 2
+    % Creating log file and setting command window level
+    L = log4matlab('logFile.log');
+    L.SetCommandWindowLevel(L.DEBUG);
+
+    % Creating aubo i5
+    r = AuboI5(eye(4),L);
+end
+
+pause;
