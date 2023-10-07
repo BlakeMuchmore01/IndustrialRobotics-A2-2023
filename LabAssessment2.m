e1a0c5f90d02cca3cc73fe4ac69dc6b8b7f54a1e
BlakeMuchmore01 <125611059+BlakeMuchmore01@users.noreply.github.com>
2023-10-07 21:22:30 +1100

    Making so concrete pose is determined by aubo origin

--- LabAssessment2.m
+++ LabAssessment2.m
@@ -9,31 +9,49 @@ function LabAssessment2()
     L.SetCommandWindowLevel(L.DEBUG);
 
     % Creating a struct of constants used within the demo
-    constants = struct('numFingers', 2);
+    constants = struct('numFingers', 2, ...
+        'axis', [-1.25 1.25 -1.25 1.25 -0.76 1.5], ...
+        'auboOrigin', eye(4));
 
     % Creating the GUI object
     guiWindow = GUI;
-    pause;
+    guiWindow.LoadLogFile(L);
     L.mlog = {L.DEBUG,'LabAssessment2','GUI page generated'};
 
-    %% Creating the robots and surrounding environment
+    %% Creating the robot's surrounding environment
     figure(1); % Creating figure to simulate robots
-    hold on; axis([-1.25 1.25 -1.25 1.25 -0.76 1.5]);
+    hold on; axis(constants.axis);
+    
+    % Calculate the rotation matrix from auboOrigin
+    rotationMatrix = constants.auboOrigin(1:3, 1:3);
+    
+    % Define the original concrete coordinates
+    concreteX = [-1.25,-1.25;1.25,1.25];
+    concreteY = [-1.25,1.25;-1.25,1.25];
+    concreteZ = [-0.76,-0.76;-0.76,-0.76];
+    
+    % Apply the rotation to the concrete coordinates
+    rotatedConcreteX = rotationMatrix(1, 1) * concreteX + rotationMatrix(1, 2) * concreteY;
+    rotatedConcreteY = rotationMatrix(2, 1) * concreteX + rotationMatrix(2, 2) * concreteY;
+    rotatedConcreteZ = concreteZ;
+    
+    % Translate the rotated coordinates based on auboOrigin
+    concreteXTranslated = rotatedConcreteX + constants.auboOrigin(1, 4);
+    concreteYTranslated = rotatedConcreteY + constants.auboOrigin(2, 4);
+    concreteZTranslated = rotatedConcreteZ + constants.auboOrigin(3, 4);
 
-    % Spawning environment components
+    % Spawning the environmental objects
     table = PlaceObject('BlackjackTable.ply', [0 0 0]); %#ok<NASGU>
     eStop = PlaceObject('EStop.ply', [-0.17 -0.3 -0.2]); %#ok<NASGU>
     fireExtinguisher = PlaceObject('FireExtinguisher.ply', [-1.75 1.65 -1.3]); %#ok<NASGU>
     
-    % Plotting the concrete floor
-    concreteX = [-1.25,-1.25;1.25,1.25];
-    concreteY = [-1.25,1.25;-1.25,1.25];
-    concreteZ = [-0.76,-0.76;-0.76,-0.76];
-    surf(concreteX,concreteY,concreteZ, ...
-        'CData',imread('concrete.jpg'),'FaceColor','texturemap');
-
+    % Plot the concrete floor
+    surf(concreteXTranslated, concreteYTranslated, concreteZTranslated, ...
+        'CData', imread('concrete.jpg'), 'FaceColor', 'texturemap');
+    
+    %% Spawning the Robots and Grippers
     % Spawning the Aubo i5 and associated 2F-85 gripper
-    auboI5 = AuboI5(eye(4),L);
+    auboI5 = AuboI5(constants.auboOrigin,L);
     auboI5.UpdateToolTr; % Updating end-effector transform property
     
     % Creating 2F-85 gripper and attaching it to the Aubo i5 end-effector
@@ -43,7 +61,7 @@ function LabAssessment2()
     end
 
     % Spawning the Dobot Magician and associated suction gripper
-    dobotMagician = DobotMagician(eye(4)*transl(0,0.3,0));
+    dobotMagician = DobotMagician(constants.auboOrigin*transl(0,0.3,0));
 
     pause;
 end
