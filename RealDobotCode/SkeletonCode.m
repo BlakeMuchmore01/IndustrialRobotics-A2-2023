8062eb843dcb8b58a1485ba2cd2225e44e1225e5
BlakeMuchmore01 <125611059+BlakeMuchmore01@users.noreply.github.com>
2023-10-16 11:57:50 +1100

    Getting skeleton code for real dobot use

---
+++ RealDobotCode/SkeletonCode.m
@@ -0,0 +1,80 @@
+%% Real Dobot Manipulation Demo Code
+rosinit('192.168.0.253');
+
+%% Getting the current safety status of the robot
+safetyStatusSubscriber = rossubscriber('/dobot_magician/safety_status');
+pause(2); %Allow some time for MATLAB to start the subscriber
+currentSafetyStatus = safetyStatusSubscriber.LatestMessage.Data;
+
+% Invalid = 0
+% Disconnected = 1
+% Initialising = 2
+% Estopped = 3
+% Operating = 4
+% Paused = 5
+% Stopped = 6
+
+%% Initialising dobot
+[safetyStatePublisher,safetyStateMsg] = rospublisher('/dobot_magician/target_safety_status');
+safetyStateMsg.Data = 2;
+send(safetyStatePublisher,safetyStateMsg);
+
+%% E-stop dobot
+% [safetyStatePublisher,safetyStateMsg] = rospublisher('/dobot_magician/target_safety_status');
+% safetyStateMsg.Data = 3;
+% send(safetyStatePublisher,safetyStateMsg);
+
+%% Getting current joint state
+jointStateSubscriber = rossubscriber('/dobot_magician/joint_states'); % Create a ROS Subscriber to the topic joint_states
+pause(2); % Allow some time for a message to appear
+currentJointState = jointStateSubscriber.LatestMessage.Position % Get the latest message
+
+%% Getting currrent end effector pose
+endEffectorPoseSubscriber = rossubscriber('/dobot_magician/end_effector_poses'); % Create a ROS Subscriber to the topic end_effector_poses
+pause(2); %Allow some time for MATLAB to start the subscriber
+currentEndEffectorPoseMsg = endEffectorPoseSubscriber.LatestMessage;
+% Extract the position of the end effector from the received message
+currentEndEffectorPosition = [currentEndEffectorPoseMsg.Pose.Position.X,
+                              currentEndEffectorPoseMsg.Pose.Position.Y,
+                              currentEndEffectorPoseMsg.Pose.Position.Z];
+% Extract the orientation of the end effector
+currentEndEffectorQuat = [currentEndEffectorPoseMsg.Pose.Orientation.W,
+                          currentEndEffectorPoseMsg.Pose.Orientation.X,
+                          currentEndEffectorPoseMsg.Pose.Orientation.Y,
+                          currentEndEffectorPoseMsg.Pose.Orientation.Z];
+% Convert from quaternion to euler
+[roll,pitch,yaw] = quat2eul(currentEndEffectorQuat);
+
+%% Setting target joint state
+jointTarget = [0,0.4,0.3,0]; % Remember that the Dobot has 4 joints by default.
+
+[targetJointTrajPub,targetJointTrajMsg] = rospublisher('/dobot_magician/target_joint_states');
+trajectoryPoint = rosmessage("trajectory_msgs/JointTrajectoryPoint");
+trajectoryPoint.Positions = jointTarget;
+targetJointTrajMsg.Points = trajectoryPoint;
+
+send(targetJointTrajPub,targetJointTrajMsg);
+
+%% Setting target end-effector state
+endEffectorPosition = [0.2,0,0.1];
+endEffectorRotation = [0,0,0];
+
+[targetEndEffectorPub,targetEndEffectorMsg] = rospublisher('/dobot_magician/target_end_effector_pose');
+
+targetEndEffectorMsg.Position.X = endEffectorPosition(1);
+targetEndEffectorMsg.Position.Y = endEffectorPosition(2);
+targetEndEffectorMsg.Position.Z = endEffectorPosition(3);
+
+qua = eul2quat(endEffectorRotation);
+targetEndEffectorMsg.Orientation.W = qua(1);
+targetEndEffectorMsg.Orientation.X = qua(2);
+targetEndEffectorMsg.Orientation.Y = qua(3);
+targetEndEffectorMsg.Orientation.Z = qua(4);
+
+send(targetEndEffectorPub,targetEndEffectorMsg);
+
+%% Setting suction cup
+% Turn on the tool
+[toolStatePub, toolStateMsg] = rospublisher('/dobot_magician/target_tool_state');
+toolStateMsg.Data = [1]; %#ok<NBRAK2> % Send 1 for on and 0 for off 
+send(toolStatePub,toolStateMsg);
