738e7c6ee60df3f867748f1451c15621ca30e94e
BlakeMuchmore01 <125611059+BlakeMuchmore01@users.noreply.github.com>
2023-10-25 23:25:47 +1100

    Update LabAssessment2.m

--- LabAssessment2.m
+++ LabAssessment2.m
@@ -55,26 +55,13 @@ classdef LabAssessment2 < handle
                     % Functionality for the blackjack card distribution
                     case "Blackjack"
                         
+                        
                     % Functionality for the teaching/jogging
                     case "Teach"
-                        % Looping functionality until end demonstration or mode is switched in the GUI
-                        while (~guiWindow.EndDemonstration || guiWindow.simulationMode == "Teach") 
-                            
-
-
-
-
-
-
-
-
-
-
-                            drawnow; % Allowing the GUI properties to be updated within the loop
-                        end
+                        
                 end
                 
-                pause(0.1); % Pausing to reduce business of the while loop
+                pause(0.5); % Pausing to reduce business of the while loop
                 drawnow; % Allowing the GUI properties to be updated within the loop
             end
 
