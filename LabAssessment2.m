52109c3580aa023822d0d4ed4c015ead2b32fa44
BlakeMuchmore01 <125611059+BlakeMuchmore01@users.noreply.github.com>
2023-10-27 16:27:32 +1100

    adding jogging joints

--- LabAssessment2.m
+++ LabAssessment2.m
@@ -79,7 +79,24 @@ classdef LabAssessment2 < handle
 
                 % Teach and Jogging Functionality
                 case "Teach"
-
+                    % Blackjack and teach functionality
+                    while ~guiWindow.endDemonstration
+                        % Getting the robot that is being controlled by the user
+                        drawnow;
+                        switch guiWindow.RobotDropDown.Value
+                            case "Aubo I5"
+                                % Getting the updated joint angles and animating
+                                auboJointAngles = guiWindow.moveAuboDeg;
+                                auboI5.model.animate(deg2rad(auboJointAngles));
+                                drawnow;
+                            case "Dobot Magician"
+                                % Getting the updated joint angles and animating
+                                dobotJointAngles = guiWindow.moveDobotDeg;
+                                dobotMagician.model.animate(deg2rad(dobotJointAngles));
+                                drawnow;
+                        end
+                        drawnow;
+                    end
             end
 
             guiWindow.delete(); % Closing the GUI wind
